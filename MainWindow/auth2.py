8f58c2eab2141e6cba707fe21d13940d1a09f281
Lucas Motta <110114441+lucassmtt@users.noreply.github.com>
2023-02-09 12:10:17 -0300

    Update auth2.py

--- MainWindow/auth2.py
+++ MainWindow/auth2.py
@@ -1,6 +1,3 @@
-import sqlite3
-
-
 class Auth:
     def __init__(self, Nome, Postname, Email, Phone):
         from pathlib import Path
@@ -12,7 +9,7 @@ class Auth:
         self.phone = Phone
         self.PATH_DB = Path().absolute() / 'DB' / 'mybase.db'
 
-        self.connect = sql.connect(self.PATH_DB)
+        self.connect = self.sql.connect(self.PATH_DB)
         self.cursor = self.connect.cursor()
         self.flag = False
 
@@ -29,37 +26,28 @@ class Auth:
             self.connect.commit()
             self.connect.close()
 
-        except sql.OperationalError:
-            print('Falha ou tabela criada')
+        except Exception as e:
+            print(f'ERROR: {e}')
 
 
     def db_check(self):
         comando = f"""
-        SELECT email FROM users
-        WHERE email = '{self.email}'
+        SELECT 'lio' FROM users
 """
         self.cursor.execute(comando)
-        self.cursor.fetchone()
-
-        # if i[0] == self.email:
-        #     print('Email criado')
-        #     self.flag = True
-        #     return self.flag
-        # else:
-        #     return self.flag
-
-
-    def db_insert(self):
-        if self.flag:
-            pass
-        else:
-            comando = f"""
-            INSERT INTO users (name, phone, email)
-            VALUES ('{self.nome_completo}', '{self.phone}', '{self.email}')
-            """
-            self.cursor.execute(comando)
-            self.connect.commit()
-            self.connect.close()
+        for c in self.cursor.fetchone():
+            print(c)
+    # def db_insert(self):
+    #     if self.flag:
+    #         pass
+    #     else:
+    #         comando = f"""
+    #         INSERT INTO users (name, phone, email)
+    #         VALUES ('{self.nome_completo}', '{self.phone}', '{self.email}')
+    #         """
+    #         self.cursor.execute(comando)
+    #         self.connect.commit()
+    #         self.connect.close()
 
 
 
